2b7bb651c24a425322345ff08fa0ecc2529075c7
E.M. Ravidi Rasanjalee Ekanayaka <164542369+RavidiEkanayaka@users.noreply.github.com>
2024-05-24 13:08:25 +0530

    Add files via upload

---
+++ dbo.UserInfo.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[UserInfo] (
+    [First Name]       CHAR (10)    NULL,
+    [Last Name]        NCHAR (10)   NULL,
+    [Contact]          INT          NOT NULL,
+    [Address]          VARCHAR (50) NULL,
+    [User Name]        NCHAR (10)   NULL,
+    [Password]         VARCHAR (6)  NULL,
+    [Confirm Password] VARCHAR (6)  NULL, 
+    CONSTRAINT [PK_UserInfo] PRIMARY KEY ([Contact])
+);
+
